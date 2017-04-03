ee4825bae35fb3e171d9f50dcd317d4a9b7fa886
Wildhoney <adam.timberlake@gmail.com>
2017-04-03 21:43:22 +0100

    Build :package:

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     entry: 'src/exact-order.js',
     dest: 'dist/exact-order.js',
     format: 'cjs',
     plugins: [
+        commonjs({
+            namedExports: {
+                'node_modules/object-path': ['default']
+            }
+        }),
         resolve({
             jsnext: true,
             module: true,
