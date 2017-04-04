94c4f03cfac3347d097dff491275d382a9360fdd
Wildhoney <adam.timberlake@gmail.com>
2017-04-04 08:56:35 +0100

    Added optional 'sort' function for omitting explicits

--- src/exact-order.js
+++ src/exact-order.js
@@ -1,34 +1,76 @@
 import path from 'object-path';
+import Symbol from 'es6-symbol';
+
+/**
+ * @constant HEAD
+ * @type {Symbol}
+ */
+export const HEAD = Symbol('set-order/head');
+
+/**
+ * @constant TAIL
+ * @type {Symbol}
+ */
+export const TAIL = Symbol('set-order/tail');
+
+/**
+ * @method handle
+ * @param {*} a
+ * @param {*} b
+ * @return {Function}
+ */
+const handle = (a, b) => {
+
+    /**
+     * @param {Function} indexOf
+     * @param {Function} sort
+     * @param {Symbol} position
+     * @return {Number|Boolean}
+     */
+    return (indexOf, sort, position) => {
+
+        const [firstIndex, secondIndex] = [indexOf(a), indexOf(b)];
+
+        if (firstIndex !== -1 && secondIndex === -1) return position === HEAD ? -1 : 1;
+        if (firstIndex === -1 && secondIndex !== -1) return position === HEAD ? 1 : -1;
+        if (firstIndex === -1 && secondIndex === -1) return sort(a, b);
+
+        return firstIndex > secondIndex;
+
+    };
+
+};
 
 /**
  * @method byPrimitive
  * @param {Array} order
+ * @param {Function} sort
+ * @param {Symbol} position
  * @return {Function}
  */
-const byPrimitive = order => (a, b) => {
-    const [firstIndex, secondIndex] = [order.indexOf(a), order.indexOf(b)];
-    return firstIndex > secondIndex;
+const byPrimitive = (order, sort, position) => (a, b) => {
+    const indexOf = x => order.indexOf(x);
+    return handle(a, b)(indexOf, sort, position);
 };
 
 /**
  * @method byNested
  * @param {Object} order
+ * @param {Function} sort
+ * @param {Symbol} position
  * @return {Function}
  */
-const byNested = order => {
+const byNested = (order, sort, position) => {
 
     const keys = Object.keys(order);
 
-    return function sort(a, b, index = 0) {
+    return function recurse(a, b, index = 0) {
 
         const key = keys[index];
+        const indexOf = x => order[key].findIndex(y => y === path.get(x, key));
+        const shouldRecurse = indexOf(a) === indexOf(b) && keys[index + 1];
 
-        const [firstIndex, secondIndex] = [
-            order[key].findIndex(x => x === path.get(a, key)),
-            order[key].findIndex(x => x === path.get(b, key))
-        ];
-
-        return firstIndex === secondIndex && keys[index + 1] ? sort(a, b, index + 1) : firstIndex > secondIndex;
+        return shouldRecurse ? recurse(a, b, index + 1) : handle(a, b)(indexOf, sort, position);
 
     };
 
@@ -36,6 +78,10 @@ const byNested = order => {
 
 /**
  * @param {Array|Object} order
+ * @param {Function} [sort = () => {}]
+ * @param {Symbol} [position = HEAD]
  * @return {Function}
  */
-export default order => Array.isArray(order) ? byPrimitive(order) : byNested(order);
+export default (order, sort = () => {}, position = HEAD) => {
+    return Array.isArray(order) ? byPrimitive(order, sort, position) : byNested(order, sort, position);
+};
--- tests/exact-order.test.js
+++ tests/exact-order.test.js
@@ -1,50 +1,113 @@
 import test from 'ava';
-import as from '../src/exact-order';
+import as, { HEAD, TAIL } from '../src/exact-order';
+import by from 'sort-by';
+
+test.beforeEach(t => {
+
+    t.context.singleCountries = [
+        { name: 'United Kingdom' },
+        { name: 'Russian Federation' },
+        { name: 'Hong Kong' },
+        { name: 'Brazil' },
+        { name: 'The Netherlands' },
+        { name: 'Zambia' },
+        { name: 'Philippines' },
+    ];
+
+    t.context.multipleCountries = [
+        { name: 'Russian Federation', leader: 'Vladimir Putin' },
+        { name: 'United Kingdom', leader: 'Margaret Thatcher' },
+        { name: 'United Kingdom', leader: 'Theresa May' },
+        { name: 'United Kingdom', leader: 'Gordon Brown' }
+    ];
+
+});
 
 test('Should be able to custom sort on primitives;', t => {
     t.deepEqual([1, 2, 3].sort(as([2, 1, 3])), [2, 1, 3]);
     t.deepEqual(['One', 'Two', 'Three'].sort(as(['Two', 'Three', 'One'])), ['Two', 'Three', 'One']);
 });
 
+test('Should be able to custom sort on primitives using natural sort;', t => {
+    t.deepEqual([1, 3, 5, 2, 4].sort(as([3], (a, b) => a > b)), [3, 1, 2, 4, 5]);
+    t.deepEqual([1, 3, 5, 2, 4].sort(as([3], (a, b) => a > b, HEAD)), [3, 1, 2, 4, 5]);
+    t.deepEqual([1, 3, 5, 2, 4].sort(as([3], (a, b) => a > b, TAIL)), [1, 2, 4, 5, 3]);
+});
+
 test('Should be able to custom sort on nested objects;', t => {
 
-    const countries = [
-        { name: 'Russian Federation' },
-        { name: 'United Kingdom' },
-        { name: 'The Netherlands' }
-    ];
-
-    countries.sort(as({
-        name: ['United Kingdom', 'The Netherlands', 'Russian Federation']
+    t.context.singleCountries.sort(as({
+        name: ['United Kingdom', 'The Netherlands', 'Russian Federation', 'Zambia', 'Hong Kong', 'Philippines', 'Brazil']
     }));
 
-    t.deepEqual(countries, [
+    t.deepEqual(t.context.singleCountries, [
         { name: 'United Kingdom' },
         { name: 'The Netherlands' },
-        { name: 'Russian Federation' }
+        { name: 'Russian Federation' },
+        { name: 'Zambia' },
+        { name: 'Hong Kong' },
+        { name: 'Philippines' },
+        { name: 'Brazil' }
+    ]);
+
+});
+
+test('Should be able to custom sort on nested objects using natural sort;', t => {
+
+    t.context.singleCountries.sort(as({ name: ['United Kingdom', 'Hong Kong'] }, by('name')));
+
+    t.deepEqual(t.context.singleCountries, [
+        { name: 'United Kingdom' },
+        { name: 'Hong Kong' },
+        { name: 'Brazil' },
+        { name: 'Philippines' },
+        { name: 'Russian Federation' },
+        { name: 'The Netherlands' },
+        { name: 'Zambia' }
+    ]);
+
+    t.context.singleCountries.sort(as({ name: ['United Kingdom', 'Hong Kong'] }, by('name'), TAIL));
+
+    t.deepEqual(t.context.singleCountries, [
+        { name: 'Brazil' },
+        { name: 'Philippines' },
+        { name: 'Russian Federation' },
+        { name: 'The Netherlands' },
+        { name: 'Zambia' },
+        { name: 'United Kingdom' },
+        { name: 'Hong Kong' }
     ]);
 
 });
 
 test('Should be able to custom sort on nested objects with multiple properties;', t => {
 
-    const countries = [
-        { name: 'Russian Federation', leader: 'Vladimir Putin' },
-        { name: 'United Kingdom', leader: 'Margaret Thatcher' },
-        { name: 'United Kingdom', leader: 'Gordon Brown' },
-        { name: 'United Kingdom', leader: 'Theresa May' },
-    ];
-
-    countries.sort(as({
+    t.context.multipleCountries.sort(as({
         name: ['Russian Federation', 'United Kingdom'],
         leader: ['Theresa May', 'Gordon Brown', 'Margaret Thatcher']
     }));
 
-    t.deepEqual(countries, [
+    t.deepEqual(t.context.multipleCountries, [
         { name: 'Russian Federation', leader: 'Vladimir Putin' },
         { name: 'United Kingdom', leader: 'Theresa May' },
         { name: 'United Kingdom', leader: 'Gordon Brown' },
         { name: 'United Kingdom', leader: 'Margaret Thatcher' }
+    ]);
+
+});
+
+test('Should be able to custom sort on nested objects with multiple properties using natural sort;', t => {
+
+    t.context.multipleCountries.sort(as({
+        name: ['United Kingdom'],
+        leader: ['Margaret Thatcher']
+    }, by('name', 'leader')));
+
+    t.deepEqual(t.context.multipleCountries, [
+        { name: 'United Kingdom', leader: 'Margaret Thatcher' },
+        { name: 'United Kingdom', leader: 'Gordon Brown' },
+        { name: 'United Kingdom', leader: 'Theresa May' },
+        { name: 'Russian Federation', leader: 'Vladimir Putin' }
     ]);
 
 });
